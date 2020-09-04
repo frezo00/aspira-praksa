9afc1f7331664e886ea282ea76acfa4216de101a
Frano Rezo <frezo@obi4wan.com>
2020-09-04 17:28:32 +0200

    More DOM methods explaining

--- js/index.js
+++ js/index.js
@@ -1,20 +1,43 @@
 let isSidebarOpen = false;
 
 function onSidebarToggle() {
-  const sidebarRef = document.getElementById("sidebar-container");
-  isSidebarOpen = !isSidebarOpen;
+  // const sidebarRef = document.getElementById("sidebar-container");
+  // Umjesto "getElementById" može se koristiti "querySelector"
+  const sidebarRef = document.querySelector("#sidebar-container");
+  sidebarRef.classList.toggle("sidebar--open");
 
-  if (isSidebarOpen) {
-    sidebarRef.classList.add("sidebar--open");
-  } else {
-    sidebarRef.classList.remove("sidebar--open");
-  }
+  // // truthy/falsy
+  // // Primjer 1: Ternarni operator: varijabla ? A : B;
+  // isSidebarOpen = !isSidebarOpen;
+  // isSidebarOpen
+  //   ? sidebarRef.classList.add("sidebar--open")
+  //   : sidebarRef.classList.remove("sidebar--open");
+
+  // // Primjer 2: Standardni if-else
+  // if (isSidebarOpen) {
+  //   sidebarRef.classList.add("sidebar--open");
+  // } else {
+  //   sidebarRef.classList.remove("sidebar--open");
+  // }
+}
+
+const boxes = document.querySelectorAll(".box"); // boxes je lista html elemenata
+
+for (let i = 0; i < boxes.length; i++) {
+  const box = boxes[i];
+  box.addEventListener("click", onBoxClick);
+}
+
+function onBoxClick(event) {
+  const boxRef = event.currentTarget; // u boxRef je spremljen: document.getElementById("blue-box")
+
+  onDuplicateBox(boxRef.id);
 }
 
 function onDuplicateBox(id) {
   const elementRef = document.getElementById(id);
+  const newBox = elementRef.cloneNode();
 
-  document.getElementById("box-container").append(elementRef.cloneNode());
+  newBox.addEventListener("click", onBoxClick);
+  document.getElementById("box-container").append(newBox);
 }
-
-console.log(document.getElementById("box-container"));
