677635f6a23ea46f29e8973fff39c88935c5bbc6
Frano Rezo <frezo@obi4wan.com>
2020-09-09 15:20:28 +0200

    API: Simple NodeJS/Express REST API server

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const studentsRouter = require("./routes/students.router");
+const { PORT } = require("./configs/environment.config");
+
+const app = express();
+
+// Middlewares
+app.use(cors()); // Enables all CORS requests
+app.use(helmet()); // Security
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // Content-Type: application/json
+
+// Routes
+app.use("/api/students", studentsRouter);
+app.get("/", (req, res, next) => {
+  res.status(404).send("<h1>Invalid route</h1>");
+});
+
+// Starting a server
+app.listen(PORT, () => console.log(`Listening to port: ${PORT}`));
